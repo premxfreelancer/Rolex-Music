d74bd553d96b064f64af3c909bbc3a5d1d1f8f72
Prem <premruidas123@gmail.com>
2024-02-24 19:56:28 +0530

    h

---
+++ script.js
@@ -0,0 +1,16 @@
+function playAudio() { 
+    var x = document.getElementById("audio1"); 
+    if (x.paused) {
+      x.play(); 
+    } else {
+      x.pause();
+    }
+  } 
+function playAudio2() { 
+    var x = document.getElementById("audio2"); 
+    if (x.paused) {
+      x.play(); 
+    } else {
+      x.pause();
+    }
+  } 
